25067ed2d12ab9e18d7bfb7f02cba7e4111908f3
bathalasupraja <bathalasupraja2@gmail.com>
2024-11-20 15:26:45 +0530

    Initial commit
    * Displayed Apple map and located user current location.
    *Added Launch screen image

--- AppleMaps/ViewController.swift
+++ AppleMaps/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var locationManger = CLLocationManager()
+    
+    
+    let mapView : MKMapView = {
+        let map = MKMapView()
+        map.overrideUserInterfaceStyle = .light
+        map.isZoomEnabled = true
+        map.isScrollEnabled = true
+        return map
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setMapConstraints()
+        mapView.delegate = self
+        locationManger.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        mapView.mapType = .standard
+        locationManger.requestWhenInUseAuthorization()
+        locationManger.requestAlwaysAuthorization()
+        locationManger.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func setMapConstraints() {
+        view.addSubview(mapView)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    }
 }
+
+
 
